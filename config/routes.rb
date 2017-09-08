93e7063af712b3529d5bdc24cabb2be2dedd2db1
Justin Walker <jrwalker314@yahoo.com>
2017-09-08 08:50:14 +0000

    Add user profile, Gravatar support, and factories for spec simplification

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,8 @@ Rails.application.routes.draw do
     post '/down-vote' => 'votes#down_vote', as: :down_vote
   end
   
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
+  
   get 'about' => 'welcome#about'
   
   resources :sessions, only: [:new, :create, :destroy]
