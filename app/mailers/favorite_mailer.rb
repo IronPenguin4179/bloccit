e43e30269881495089c581094f642dabacaab660
Justin Walker <jrwalker314@yahoo.com>
2017-09-09 05:12:53 +0000

    Favorite new post and send email notification

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -12,4 +12,15 @@ class FavoriteMailer < ApplicationMailer
         
         mail(to: user.email, subject: "New comment on #{post.title}")
     end
+    
+    def new_post(post)
+        headers["Message-ID"] = "<posts/#{post.id}@blooming-inlet-80859.herokuapp.com>"
+        headers["In-Reply-To"] = "<post/#{post.id}@blooming-inlet-80859.herokuapp.com>"
+        headers["References"] = "<post/#{post.id}@blooming-inlet-80859.herokuapp.com>"
+        
+        @post = post
+        
+        mail(to: post.user.email, subject: "You're following, #{post.title}!")
+    
+    end
 end
